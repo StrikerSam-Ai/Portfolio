71668cba04d6b46e3940823dc18297cd26112268
StrikerSam-Ai <shashwatmishra0369@gmail.com>
2025-07-11 00:51:59 +0530

    feat: modify App.css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 import AboutSection from './AboutSection'
+import PortfolioSection from './PortfolioSection'
 
 function App() {
   return (
-    <>
+    <div className="main-content">
       <main className="hero-section">
         {/* Placeholder for your professional photo or avatar */}
         <div className="hero-photo">[Your Photo]</div>
@@ -15,9 +16,16 @@ function App() {
         <button className="hero-cta">Contact Me</button>
         {/* Brief description */}
         <p className="hero-description">[Brief description of what you do]</p>
+        {/* SVG Wave Transition */}
+        <div className="hero-wave">
+          <svg viewBox="0 0 1440 120" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+            <path d="M0,40 C480,160 960,0 1440,80 L1440,120 L0,120 Z" fill="#fff" />
+          </svg>
+        </div>
       </main>
       <AboutSection />
-    </>
+      <PortfolioSection />
+    </div>
   )
 }
 
